9bd90e16979c74413671b7560d631fcf6287b5ab
Daniel Freitas <contatodanielfreitasce@gmail.com>
2023-10-08 18:17:29 -0300

    Modulo 03

--- core/crud.ts
+++ core/crud.ts
@@ -38,7 +38,7 @@ function create(content: string): Todo {
     return todo;
 }
 
-function read(): Array<Todo> {
+export function read(): Array<Todo> {
     const dbString = fs.readFileSync(DB_FILE_PATH, "utf-8");
     const db = JSON.parse(dbString || "{}");
     if (!db.todos) {
@@ -120,5 +120,5 @@ const thirdTodo = create("Terceira TODO");
 // });
 updateContentById(thirdTodo.id, "Atualizada!");
 const todos = read();
-console.log(todos);
-console.log(todos.length);
+//console.log(todos);
+//console.log(todos.length);
