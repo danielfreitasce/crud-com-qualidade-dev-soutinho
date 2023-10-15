aa25b4ecb9c6eec4efa3b390d9e327a554ba906b
Daniel Freitas <contatodanielfreitasce@gmail.com>
2023-10-15 11:19:18 -0300

    Add controllers

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,23 @@
 import React from "react";
 import { GlobalStyles } from "@ui/theme/GlobalStyles";
+import { todoController } from "@ui/controller/todo";
 
 const bg = "https://mariosouto.com/cursos/crudcomqualidade/bg";
 
+interface HomeTodo {
+    id: string;
+    content: string;
+}
+
 export default function Page() {
+    const [todos, setTodos] = React.useState<HomeTodo[]>([]);
+
+    React.useEffect(() => {
+        todoController.get().then((todos) => {
+            setTodos(todos);
+        });
+    }, []);
+
     return (
         <main>
             <GlobalStyles themeName="devsoutinho" />
@@ -44,60 +58,51 @@ export default function Page() {
                     </thead>
 
                     <tbody>
-                        <tr>
-                            <td>
-                                <input type="checkbox" />
-                            </td>
-                            <td>d4f26</td>
-                            <td>
-                                Conteúdo de uma TODO Lorem ipsum dolor sit amet
-                                consectetur adipisicing elit. Eaque vero facilis
-                                obcaecati, autem aliquid eius! Consequatur eaque
-                                doloribus laudantium soluta optio odit,
-                                provident, ab voluptates doloremque voluptas
-                                recusandae aspernatur aperiam.
-                            </td>
-                            <td align="right">
-                                <button data-type="delete">Apagar</button>
-                            </td>
-                        </tr>
+                        {todos.map((currentTodo) => {
+                            return (
+                                <tr key={currentTodo.id}>
+                                    <td>
+                                        <input type="checkbox" />
+                                    </td>
+                                    <td>{currentTodo.id.substring(0, 4)}</td>
+                                    <td>{currentTodo.content}</td>
+                                    <td align="right">
+                                        <button data-type="delete">
+                                            Apagar
+                                        </button>
+                                    </td>
+                                </tr>
+                            );
+                        })}
 
-                        <tr>
-                            <td
-                                colSpan={4}
-                                align="center"
-                                style={{ textAlign: "center" }}
-                            >
-                                Carregando...
-                            </td>
-                        </tr>
+                        {/* <tr>
+              <td colSpan={4} align="center" style={{ textAlign: "center" }}>
+                Carregando...
+              </td>
+            </tr> */}
 
-                        <tr>
-                            <td colSpan={4} align="center">
-                                Nenhum item encontrado
-                            </td>
-                        </tr>
+                        {/* <tr>
+              <td colSpan={4} align="center">
+                Nenhum item encontrado
+              </td>
+            </tr> */}
 
-                        <tr>
-                            <td
-                                colSpan={4}
-                                align="center"
-                                style={{ textAlign: "center" }}
-                            >
-                                <button data-type="load-more">
-                                    Carregar mais{" "}
-                                    <span
-                                        style={{
-                                            display: "inline-block",
-                                            marginLeft: "4px",
-                                            fontSize: "1.2em",
-                                        }}
-                                    >
-                                        ↓
-                                    </span>
-                                </button>
-                            </td>
-                        </tr>
+                        {/* <tr>
+              <td colSpan={4} align="center" style={{ textAlign: "center" }}>
+                <button data-type="load-more">
+                  Carregar mais{" "}
+                  <span
+                    style={{
+                      display: "inline-block",
+                      marginLeft: "4px",
+                      fontSize: "1.2em",
+                    }}
+                  >
+                    ↓
+                  </span>
+                </button>
+              </td>
+            </tr> */}
                     </tbody>
                 </table>
             </section>
